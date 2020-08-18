167adad006a0f7c496ad43083a1ae244625f01a9
Eugenia <eugeniasol13@gmail.com>
2020-08-18 12:14:45 -0400

    initial commit

---
+++ utils/index.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
